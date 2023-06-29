45a09f370debac13491abc7a9724c7ec55b852fd
kyomi <me@kyomi.dev>
2023-06-29 18:41:30 -0300

    style: fix flake8 and mypy issues

--- manage.py
+++ manage.py
@@ -4,9 +4,10 @@ import os
 import sys
 
 
-def main():
+def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +16,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
--- server/asgi.py
+++ server/asgi.py
@@ -1,10 +1,15 @@
 """
-ASGI config for server project.
+Copyright (C) 2023 Mojito Team
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 """
 
 import os
