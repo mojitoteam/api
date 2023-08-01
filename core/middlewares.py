3cf81b0d1d5142494ebf0281bf6a7186f99ebc6e
kyomi <me@kyomi.dev>
2023-07-31 23:13:05 -0300

    feat(server): add a custom middleware to remove useless headers

---
+++ core/middlewares.py
@@ -0,0 +1,34 @@
+"""
+Copyright (C) 2023 Mojito Team
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+from typing import Callable
+
+from django.http import HttpRequest, HttpResponse
+
+
+class RemoveHeaders:
+    """Middleware to remove some headers from the response."""
+
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]) -> None:
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        response = self.get_response(request)
+
+        # We delete some headers that we don't want to show to the user.
+        del response["Server"]
+        del response["Vary"]
+        del response["Allow"]
+
+        return response
