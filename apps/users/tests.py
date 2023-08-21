952b11d3afd78fcb53f0a4a67b2eaa140c5b3551
kyomi <me@kyomi.dev>
2023-08-21 02:02:13 -0300

    test(apps/users): test the users endpoint

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -13,8 +13,11 @@ GNU General Public License for more details.
 """
 
 from rest_framework.reverse import reverse
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_201_CREATED, HTTP_401_UNAUTHORIZED
 from rest_framework.test import APITestCase
+
+from apps.users.models import User
+from apps.users.serializers import UserSerializer
 
 
 class SelfUserTestCase(APITestCase):
@@ -35,4 +38,52 @@ class SelfUserTestCase(APITestCase):
         expected = {"token": data["token"]}
 
         self.assertEqual(res.status_code, HTTP_201_CREATED)
+        self.assertDictEqual(res.data, expected)
+
+
+class UsersTestCase(APITestCase):
+    """Test case for the users endpoint."""
+
+    def setUp(self) -> None:
+        self.example = {
+            "username": "user",
+            "email": "user@email.com",
+            "password": "password",
+        }
+        self.user = User.objects.create_user(**self.example)
+
+    def test_get_self(self) -> None:
+        url = reverse("users:users-detail", kwargs={"pk": "me"})
+        self.client.force_authenticate(self.user)
+
+        res = self.client.get(url)
+        expected = UserSerializer(self.user)
+
+        self.assertDictEqual(res.data, expected.data)
+
+    def test_get_self_without_authentication(self) -> None:
+        url = reverse("users:users-detail", kwargs={"pk": "me"})
+
+        res = self.client.get(url)
+        expected = {"detail": "Authentication credentials were not provided."}
+
+        self.assertEqual(res.status_code, HTTP_401_UNAUTHORIZED)
+        self.assertDictEqual(res.data, expected)
+
+    def test_get_user(self) -> None:
+        url = reverse("users:users-detail", kwargs={"pk": self.user.id})
+        self.client.force_authenticate(self.user)
+
+        res = self.client.get(url)
+        expected = UserSerializer(self.user)
+
+        self.assertDictEqual(res.data, expected.data)
+
+    def test_get_user_without_authentication(self) -> None:
+        url = reverse("users:users-detail", kwargs={"pk": self.user.id})
+
+        res = self.client.get(url)
+        expected = {"detail": "Authentication credentials were not provided."}
+
+        self.assertEqual(res.status_code, HTTP_401_UNAUTHORIZED)
         self.assertDictEqual(res.data, expected)
